fea052ed5fceeba92ffb11f484ff5cddac882e39
Willian Mano <willian.mano@jove.com>
2023-03-31 15:25:52 -0300

    Add fr language

---
+++ lang/fr/portfoliogroup.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_portfoliogroup
+ * @copyright   2023 World Bank Group <https://worldbank.org>
+ * @author      Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['missingidandcmid'] = 'ID et cmid manquants';
+$string['missingportfoliowithevaluation'] = 'Ce cours n \'a pas de portfolio avec évaluation.';
+$string['modulename'] = 'Portfolio du Groupe';
+$string['modulename_help'] = 'Utilisez le module du portfolio pour...';
+$string['modulenameplural'] = 'Portfolios';
+$string['newmodulesettings'] = 'Paramètres';
+
+$string['noportfolios'] = 'Pas de portfolios';
+$string['pluginadministration'] = 'Gestion de Portfolio';
+$string['pluginname'] = 'Portfolio du groupe';
+$string['portfoliogroup:addinstance'] = 'Ajouer l \'exemple';
+$string['portfoliogroup:view'] = 'Afficher l \'exemple';
+$string['portfoliogroup:submit'] = 'Soumettre les entrées du portfolio';
+$string['portfoliogroup:grade'] = 'Portfolio de notes';
+$string['privacy:metadata'] = 'Le portfolio ne stocke aucune donnée personnelle ';
+$string['view'] = 'Afficher';
+
+$string['name'] = 'Nom';
+$string['title'] = 'Titre';
+$string['content'] = 'Contenu';
+$string['attachments'] = 'Pièces jointes';
+$string['validation:titlerequirelen'] = 'Le titre est obligatoire et doit comporter au moins 3 caractères.';
+$string['validation:contentachmentsrequired'] = 'Vous devez ajouter de texte à votre entrée ou soumettre au moins un fichier.';
+$string['validation:contentlen'] = 'Le texte du message doit comporter au moins 10 caractères';
+
+$string['entry:add'] = 'Ajouter une nouvelle entrée au portfolio';
+$string['entry:add_success'] = 'Entrée créée avec succès';
+$string['entry:update_success'] = 'Entrée mise à jour avec succès';
+
+$string['downloadfile'] = 'Téléchargez le fichier';
+$string['linkccopiedtoclipboard'] = 'Lien copié dans le presse-papiers';
+
+$string['backtolayoutchoose'] = 'Retour à la mise en page choisir';
+
+$string['viewportfolio'] = 'Consulter le portfolio';
+$string['comment'] = 'Commentaire';
+$string['writeacomment'] = '+ Partagez vos réflexions...';
+$string['editcomment'] = 'Modifier';
+$string['editcomment_success'] = 'Commentaire modifié avec succès.';
+$string['edited'] = 'Modifié ';
+
+$string['myteam'] = 'Mon groupe';
+$string['teamportfolio'] = 'Portfolio du groupe';
+$string['networkportfolio'] = 'Portfolio du réseau';
+
+$string['grade'] = 'Notes';
+$string['addgrade'] = 'Ajouter une note';
+$string['assessment'] = 'Évaluation';
+$string['notyetassessed'] = 'Pas encore évalué';
+$string['grading_success'] = 'Notation terminée avec succès.';
+
+$string['status'] = 'Statut';
+$string['submitted'] = 'Soumis';
+$string['evaluated'] = 'Évalué';
+
+$string['postedby'] = 'Posté par';
+$string['groupmembers'] = 'Membres du groupe';
+
+$string['previous'] = 'Précédent';
+$string['next'] = 'Suivant';
+$string['viewtablereport'] = 'Voir le rapport du tableau';
+$string['timeline'] = 'Calendrier';
+$string['mansory'] = 'Mansory';
+$string['blog'] = 'Blog';
+$string['previewthislayout'] = 'Visualiser cette mise en page';
+$string['savemypreferences'] = 'Sauvegarder mes préférences';
+$string['chooselayouttitle'] = 'Cher étudiant, avant de commencer à créer votre portfolio, vous devez d\'abord en choisir la mise en page.';
+$string['chooselayoutsubtitle1'] = 'Vous pouvez cliquer sur les boutons d\'aperçu pour voir comment chaque mise en page est présentée.';
+$string['chooselayoutsubtitle2'] = 'Ne vous inquiétez pas, vous pouvez changer la mise en page de votre Portfolio à tout moment.';
+$string['backtoportfolios'] = 'Retour aux portfolios';
+$string['hellothisisourportfolio'] = 'Salut, voici notre portfolio!';
+$string['portfolioempty'] = 'Portfolio vide!';
+$string['portfolioemptydesc'] = 'Il n\'y a pas encore d\'entrées pour ce portfolio, veuillez revenir plus tard.';
+$string['yourportfolioisempty'] = 'Votre portfolio est vide.';
+$string['yourportfolioisemptydesc'] = 'Cher étudiant, vous devez commencer à créer votre portfolio, cliquez sur le bouton ci-dessus pour commencer à y ajouter des entrées.';
+$string['news'] = 'Actualités';
+$string['lastentry'] = 'Dernière entrée';
+$string['entries'] = 'entrées';
+$string['likes'] = 'Likes';
+$string['comments'] = 'Commentaires';
+$string['shareportfolio'] = 'Partagez le portfolio';
+$string['usernogroup'] = 'Vous n\'êtes pas membre d\'un groupe.';
+$string['usernogroupdesc'] = 'Cher étudiant, ceci est une activité de groupe, vous devez faire partie d\'un groupe pour la faire.';
+$string['backtocampaign'] = 'Retour à la campagne';
+$string['changelayout'] = 'Changer la mise en page';
+
+$string['completionrequiresubmit'] = 'Soumission requise';
+$string['completionrequiresubmit_help'] = 'L\'utilisateur doit soumettre pour terminer cette activité';
+
+$string['editpost'] = 'Modifier le poste';
+$string['deletepost'] = 'Supprimer le poste';
+
+$string['entrydelete_confirm_title'] =' Êtes-vous sûr?';
+$string['entrydelete_confirm_msg'] = 'Une fois supprimée, cette entrée ne peut être récupérée.';
+$string['entrydelete_confirm_yes'] = 'Oui, supprimez-la.';
+$string['entrydelete_confirm_no'] = 'Annuler';
+$string['entrydelete_success'] = 'Entrée supprimée avec succès.';
